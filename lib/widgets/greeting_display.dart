8bfd552dcd822f9f3375dde0ad030983345097f6
pedrinhouhggez <pedrinhouhggez@gmail.com>
2025-11-07 22:37:03 +0000

    temp removed buttons

--- lib/widgets/greeting_display.dart
+++ lib/widgets/greeting_display.dart
@@ -10,31 +10,31 @@ class GreetingDisplay extends StatelessWidget {
       color: const Color.fromARGB(255, 212, 0, 255),
       child: Column(
         children: <Widget>[
-          Row(
-            children: [
-              const SizedBox(width: 8),
-              ButtonsModelDisplay(
-                text: 'Todos',
-                onPressed: () {
-                  print('Botão clicado!');
-                },
-              ),
-              const SizedBox(width: 8),
-              ButtonsModelDisplay(
-                text: 'Fixadas',
-                onPressed: () {
-                  print('Botão clicado!');
-                },
-              ),
-              const SizedBox(width: 8),
-              ButtonsModelDisplay(
-                text: 'Arquivadas',
-                onPressed: () {
-                  print('Botão clicado!');
-                },
-              ),
-            ],
-          ),
+          // Row(
+          //   children: [
+          //     const SizedBox(width: 8),
+          //     ButtonsModelDisplay(
+          //       text: 'Todos',
+          //       onPressed: () {
+          //         print('Botão clicado!');
+          //       },
+          //     ),
+          //     const SizedBox(width: 8),
+          //     ButtonsModelDisplay(
+          //       text: 'Fixadas',
+          //       onPressed: () {
+          //         print('Botão clicado!');
+          //       },
+          //     ),
+          //     const SizedBox(width: 8),
+          //     ButtonsModelDisplay(
+          //       text: 'Arquivadas',
+          //       onPressed: () {
+          //         print('Botão clicado!');
+          //       },
+          //     ),
+          //   ],
+          // ),
 
           Text('Coluna 2'),
           Text('Contagem de vezes que você pressionou o botão:'),
